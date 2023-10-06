fc05e6afada50eb6be9b74ee67147dc8c9c7ec9c
gapiyka <50524296+gapiyka@users.noreply.github.com>
2023-10-06 21:04:39 +0300

    add timer logic for boosts
    > timer
    > color changing for periods
    > list of randomized boosts for each cell

--- FightWorlds/Assets/Scripts/UI/TechnoMap.cs
+++ FightWorlds/Assets/Scripts/UI/TechnoMap.cs
@@ -1,35 +1,128 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class TechnoMap : MonoBehaviour
+namespace FightWorlds.UI
 {
-    [SerializeField] private Grid grid;
-    [SerializeField] private GameObject hexPrefab;
-    [SerializeField] private Vector2Int size;
-    [SerializeField] private int radius;
-    private void Start()
+    public enum BoostType
     {
-        grid.cellSize = hexPrefab.GetComponent<RectTransform>().rect.size;
-        int width = size.x / 2 - 1; // offset due to 0,0 hex
-        int height = size.y / 2 - 1;
-        for (int y = -height; y <= height; y++)
+        None = 0,
+        Damage = 1,
+        Rate = 2,
+        Range = 3,
+        Health = 4
+    }
+
+    [Serializable]
+    public class BoostCell
+    {
+        public Vector3Int GridCoords;
+        public BoostType Type;
+        public float TimeLeft;
+
+        public BoostCell(Vector3Int coordinates,
+        BoostType type, float time)
         {
-            for (int x = -width; x <= width; x++)
-            {
-                var worldPos = grid.GetCellCenterWorld(new(x, y));
-                var obj = Instantiate(hexPrefab, worldPos,
-                    Quaternion.identity, grid.transform);
-                obj.name = $"Boost {x} {y}";
-                obj.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text
-                    = $"X: {x}\nY: {y}";
-            }
+            GridCoords = coordinates;
+            Type = type;
+            TimeLeft = time;
         }
     }
 
-    private void Update()
+    [Serializable]
+    public struct BoostTimeExpire
     {
+        public float Time;
+        public Color Color;
+    }
 
+    public class TechnoMap : MonoBehaviour
+    {
+        [SerializeField] private BoostTimeExpire[] ColorByTime;
+        [SerializeField] private UnityEngine.Grid grid;
+        [SerializeField] private GameObject hexPrefab;
+        [SerializeField] private Vector2Int size;
+        [SerializeField] private int radius;
+
+        private const float maxTime = 86400; // day in sec
+        private const float addTime = 10800; // 3 hours
+
+        public List<BoostCell> BoostsList;
+
+        private void Start()
+        {
+            grid.cellSize = hexPrefab.GetComponent<RectTransform>().rect.size;
+            //BoostsList = new();
+            //int width = size.x / 2 - 1; // offset due to 0,0 hex
+            //int height = size.y / 2 - 1;
+            //for (int y = -height; y <= height; y++)
+            //{
+            //    for (int x = -width; x <= width; x++)
+            //Vector3 zeroCell = grid.GetCellCenterWorld(Vector3Int.zero);
+            foreach (Transform child in grid.transform)
+            {
+                string[] arr = child.name.Split(" ");
+                Vector3Int coords =
+                    new(Int32.Parse(arr[2]), Int32.Parse(arr[3]));
+                //var type = Enum.Parse<BoostType>(arr[0]);
+                //var worldPos = grid.GetCellCenterWorld(coords);
+                //var timeLeft = (worldPos == zeroCell ||
+                //    Vector3.Distance(worldPos, zeroCell) > radius) ? 0 :
+                //    UnityEngine.Random.Range(0, maxTime);
+                //var cell = new BoostCell(coords, type, timeLeft);
+                //BoostsList.Add(cell);
+                //TimeSpan time = TimeSpan.FromSeconds(cell.TimeLeft);
+                //child.GetChild(0).GetComponent<TextMeshProUGUI>().
+                //    text = $"X:{coords.x} Y:{coords.y}\n{type}\n" +
+                //    time.ToString("hh':'mm':'ss");
+                //ColorCell(child, cell.TimeLeft);
+                if (coords == Vector3Int.zero)
+                    continue;
+                int index = BoostsList.FindIndex(b => b.GridCoords == coords);
+                child.AddComponent<Button>().onClick.AddListener(() =>
+                {
+                    BoostsList[index].TimeLeft += addTime;
+                    if (BoostsList[index].TimeLeft > maxTime)
+                        BoostsList[index].TimeLeft = maxTime;
+                });
+            }
+            //}
+        }
+
+        private void Update()
+        {
+            int counter = -1;
+            foreach (var cell in BoostsList)
+            {
+                counter++;
+                if (cell.TimeLeft <= 0 || cell.TimeLeft < Time.deltaTime)
+                    continue;
+                cell.TimeLeft -= Time.deltaTime;
+                if (!grid.gameObject.activeSelf) continue;
+                Transform canvasCell = grid.transform.GetChild(counter);
+                ColorCell(canvasCell, cell.TimeLeft);
+                TimeSpan time = TimeSpan.FromSeconds(cell.TimeLeft);
+                canvasCell.GetChild(0).GetComponent<TextMeshProUGUI>().
+                    text = $"X:{cell.GridCoords.x} Y:{cell.GridCoords.y}\n" +
+                    $"{cell.Type}\n{time.ToString("hh':'mm':'ss")}";
+            }
+        }
+
+        private void ColorCell(Transform cell, float time)
+        {
+            Image image = cell.GetComponent<Image>();
+            Color color = image.color;
+            foreach (var pair in ColorByTime)
+            {
+                if (time > pair.Time) continue;
+                color = pair.Color;
+                break;
+            }
+            image.color = color;
+        }
     }
 }
